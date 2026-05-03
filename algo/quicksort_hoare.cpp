33779ab721fd53a88c5cb3a1c10bbb5cbe23dd35
Lâm Phạm <76936718+phamlam2k@users.noreply.github.com>
2026-05-03 03:47:57 +0000

    feat: quicksort by hoare

--- algo/quicksort_hoare.cpp
+++ algo/quicksort_hoare.cpp
@@ -1,21 +1,66 @@
-#include <iostream>
-#include <algorithm>
-#include <map>
-#include <string>
-
+#include <bits/stdc++.h>
 #define endl "\n"
 #define int long long
 #define CODEGOD ios_base::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
 
+int partition(int arr[], int l, int r) {
+    int pivot = arr[l];
+
+    int i = l - 1;
+    int j = r + 1;
+
+    while(1) {
+        do {
+            ++i;
+        } while (arr[i] < pivot && i <= r);
+        
+        do {
+            --j;
+        } while (arr[j] > pivot && j >= 0);
+
+        if (i < j) {
+            swap(arr[i], arr[j]);
+        } else {
+            break;
+        }
+    }
+
+    return j;
+}
+
+void quicksort(int arr[], int l, int r) {
+    if (l >= r) return;
+
+    int p = partition(arr, l, r);
+
+    quicksort(arr, l, p);
+
+    quicksort(arr, p + 1, r);
+}
+
 void solve() {
-    
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quicksort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 }
 
 signed main() {
     CODEGOD;
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) {
         solve();
     }
