c2aa21361dbc63f657c98c72922baa111a970068
phamlam2k <mailam1309@gmail.com>
2026-06-18 09:34:17 +0700

    feat: 56 merged interval leetcode

---
+++ leetcode/56_merged_interval.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+
+#define endl "\n"
+#define int long long
+#define CODEGOD ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+void solve() {
+    int n;
+    
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(2));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    sort(arr.begin(), arr.end());
+
+    vector<vector<int>> res;
+
+    res.push_back(arr[0]);
+
+    for (int i = 1; i < n; i++) {
+        vector<int>& selected = res.back();
+
+        int x = selected[1];
+        int y = arr[i][0];
+
+        if (x >= y) {
+            selected[1] = max(x, arr[i][1]);
+        } else {
+            res.push_back(arr[i]);
+        }
+    }
+
+    int len = res.size();
+
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << res[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+}   
+
+signed main() {
+    CODEGOD;
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+}
