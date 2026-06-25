9d9864c1973c2c9f6f293c89b1e748f28dd65dc8
phamlam2k <mailam1309@gmail.com>
2026-06-25 13:35:54 +0700

    feat: encode decode strings neetcode

---
+++ neetcode/arrays_hashing/encode_decode_strings.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+
+#define endl "\n"
+#define int long long
+#define CODEGOD ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+string encode (vector<string>& strs) {
+    int len = strs.size();
+
+    string str;
+    
+    for (int i = 0; i < len; i++) {
+        str += strs[i];
+
+        if (i < len - 1) {
+            str += "~";
+        }
+    }
+}
+
+vector<string> decode(string s) {
+    vector<string> ans;
+    int len = s.length();
+    string str = "";
+
+    for (int i = 0; i < len; i++) {
+        if (s[i] == '~') {
+            ans.push_back(str);
+            str = "";
+        } else {
+            str += s[i];
+        }
+    }
+
+    return ans;
+}
+
+void solve() {
+    
+}
+
+signed main() {
+    CODEGOD;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
