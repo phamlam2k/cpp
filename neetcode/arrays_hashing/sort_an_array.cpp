6376b079a17f2ed4aa791db9bfcc78fd23af6a03
phamlam2k <mailam1309@gmail.com>
2026-07-21 15:27:42 +0700

    feat(neetcode/array_hashing): sort an array

--- neetcode/arrays_hashing/sort_an_array.cpp
+++ neetcode/arrays_hashing/sort_an_array.cpp
@@ -1,21 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <map>
 #include <string>
+#include <vector>
 
 #define endl "\n"
 #define int long long
 #define CODEGOD ios_base::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
 
-void merge(int arr[], int l, int m, int r) {
-    vector<int> v1(arr + l, arr + m + 1);
-    vector<int> v2(arr + m + 1, arr + r + 1);
+void merge (vector<int> &arr, int l, int mid, int r) {
+    vector<int> v1(arr.begin() + l, arr.begin() + mid + 1);
+    vector<int> v2(arr.begin() + mid + 1, arr.begin() + r + 1);
 
+    int len1 = v1.size();
+    int len2 = v2.size();
     int i = 0;
     int j = 0;
 
-    while (i < v1.size() && j < v2.size())
-    {
-        if (v1[i] <= v2[j]) {
+    while (i <= len1 - 1 && j <= len2 - 1) {
+        if (v1[i] < v2[j]) {
             arr[l] = v1[i];
             i++;
         } else {
@@ -25,24 +29,24 @@ void merge(int arr[], int l, int m, int r) {
 
         l++;
     }
-    
-    while(i < v1.size()) {
+
+    while (i <= len1 - 1) {
         arr[l] = v1[i];
         l++;
         i++;
     }
 
-    while(j < v2.size()) {
+    while (j <= len2 - 1) {
         arr[l] = v2[j];
         l++;
         j++;
     }
 }
 
-void mergeSort(int arr[], int l, int r) {
+void mergeSort (vector<int> &arr, int l, int r) {
     if (l >= r) return;
 
-    int mid = (l + r) / 2;
+    int mid = l + (r - l) / 2;
 
     mergeSort(arr, l, mid);
     mergeSort(arr, mid + 1, r);
@@ -54,23 +58,19 @@ void solve() {
 
     cin >> n;
 
-    int arr[n];
+    vector<int> nums(n);
 
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> nums[i];
     }
 
-    mergeSort(arr, 0, n - 1);
 
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
 }
 
 signed main() {
     CODEGOD;
     int t = 1;
-    // cin >> t;
+    cin >> t;
     while (t--) {
         solve();
     }
